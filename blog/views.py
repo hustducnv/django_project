cf37a4cc622672d7754227833dfcee35edefea5b
hustducnv <hust.ducnv@gmail.com>
2020-08-01 19:54:55 +0700

    create usersapp

--- blog/views.py
+++ blog/views.py
@@ -1,26 +1,10 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-
-
-posts = [
-    {
-        'author': 'Ducnv',
-        'title': 'Blog post 1',
-        'content': 'First post content',
-        'date_posted': 'July 17, 2020'
-    },
-    {
-        'author': 'Longnv',
-        'title': 'Blog post 2',
-        'content': 'Second post content',
-        'date_posted': 'July 18, 2020'
-    }
-]
+from .models import Post
 
 
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
